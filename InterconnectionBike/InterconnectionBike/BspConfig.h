023bb81524a01d8361b64d691c2c3c783700127d
XiaoWenlg <daiyu0@163.com>
2021-08-01 11:16:09 +0800

    添加应用次数

--- InterconnectionBike/InterconnectionBike/BspConfig.h
+++ InterconnectionBike/InterconnectionBike/BspConfig.h
@@ -83,8 +83,8 @@ typedef struct SportInfo      //运动信息
 #define VOLTAGE_T						2000				//电压检测周期单位ms
 #define POWER_VOLTAGE_LOW				9500				//系统要求最低电压单位mV
 //不同器材不同参数
-#define CAL_K							5				//cal系数
-#define EMID         "00010000300007000090"
+#define CAL_K							2				//cal系数
+#define EMID         "00010000300023000143"
 //
 
 #endif // !__BSPCONFIG_H
